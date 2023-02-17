b8ae41434ff4aa09be8e5c933ca2a6bb7341d0a4
starikna <don.strekoza@gmail.com>
2023-02-17 21:29:04 +0700

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 // console.log('worked');
-export default function healthIndicator ( {name, health} ) {
-    let indicatorLevel = "";
+export default function healthIndicator({ health }) {
+  let indicatorLevel = '';
 
-    if (health > 50) {
-        indicatorLevel = "healthy"
-    } else if (health < 15) {
-        indicatorLevel = "critical"
-    } else {
-        indicatorLevel = "wounded"
-    }
-    return indicatorLevel;
+  if (health > 50) {
+    indicatorLevel = 'healthy';
+  } else if (health < 15) {
+    indicatorLevel = 'critical';
+  } else {
+    indicatorLevel = 'wounded';
+  }
+  return indicatorLevel;
 }
